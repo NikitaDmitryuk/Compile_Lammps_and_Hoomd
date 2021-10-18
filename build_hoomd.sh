b238390d38a64796f810508165c88a05c28a5540
NikitaDmitryuk <dmitryuk.nikita@gmail.com>
2021-10-18 16:16:42 +0300

    add write hoomd

--- build_hoomd.sh
+++ build_hoomd.sh
@@ -1,18 +1,22 @@
 #! /bin/bash
 
-name_env="EnvName"
+name_env="LJ12-6"
 anaconda_path=$HOME/anaconda3
 build_path="build"
 
-
-conda create -y -n $name_env python=3.7
+conda remove -y --name $name_env --all
+conda create -y -n $name_env python=3.9
 source ${anaconda_path}/etc/profile.d/conda.sh
 conda activate $name_env
-python3 hoomd-blue/install-prereq-headers.py
-conda install -y numpy
 
-cmake -B $build_path -S hoomd-blue -GNinja
-cmake $build_path -DENABLE_GPU=on -DCMAKE_C_COMPILER=gcc-8 -DCMAKE_C_COMPILER_AR=gcc-ar-8 -DCMAKE_C_COMPILER_RANLIB=gcc-ranlib-8 -DCMAKE_CXX_COMPILER=g++-8
+# cmake --build $build_path --target clean
+cmake -B $build_path -S hoomd-v2.9.7 -GNinja
+cmake $build_path\
+    -DCMAKE_INSTALL_PREFIX=`python3 -c "import site; print(site.getsitepackages()[0])"`\
+    -DCMAKE_CXX_FLAGS=-march=native\
+    -DCMAKE_C_FLAGS=-march=native\
+    -DENABLE_CUDA=ON
 
 cmake --build $build_path
+
 cmake --install $build_path
