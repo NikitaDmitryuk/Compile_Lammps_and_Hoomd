afca60a157ed5eeebe1c056953ecfb15c023ffd9
Никита <50713261+NikitaDmitryuk@users.noreply.github.com>
2021-11-29 13:34:30 +0300

    fix

--- build_hoomd.sh
+++ build_hoomd.sh
@@ -5,7 +5,7 @@ anaconda_path=$HOME/anaconda3
 build_path="build"
 
 conda remove -y --name $name_env --all
-conda create -y -n $name_env python=3.9
+conda create -y -n $name_env python=3.8
 source ${anaconda_path}/etc/profile.d/conda.sh
 conda activate $name_env
 conda install -y numpy
@@ -16,7 +16,10 @@ cmake $build_path\
     -DCMAKE_INSTALL_PREFIX=`python3 -c "import site; print(site.getsitepackages()[0])"`\
     -DCMAKE_CXX_FLAGS=-march=native\
     -DCMAKE_C_FLAGS=-march=native\
+    -DCMAKE_CXX_COMPILER=/usr/bin/c++-8\ #?
     -DENABLE_CUDA=ON
+
+cmake $build_path -DENABLE_CUDA=ON
 
 cmake --build $build_path
 cmake --install $build_path
